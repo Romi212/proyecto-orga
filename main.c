732dc129c96399519ae90d4f5df67a7e5e62ab03
Romi212 <104693764+Romi212@users.noreply.github.com>
2022-09-13 07:38:09 -0300

    pruebasConMainConsulta

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include "colacp.h"
 
-int main()
+int main(int nro, char** argc)
 {
-    printf("Hello Marto!\n");
+    printf("%s %d \n",argc[1],nro);
     printf("Hello Romi!\n");
 
     return 0;
